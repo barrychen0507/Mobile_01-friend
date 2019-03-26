40a8a1e9c34e4fa6147d96c4f217d32b44f698a9
barrychen0507 <36660571+barrychen0507@users.noreply.github.com>
2019-03-26 18:16:29 +0530

    Changed to the "Real" repository :100:

---
+++ index.php
@@ -0,0 +1,106 @@
+﻿<head>
+<title>皇翔租借商場 - 首頁</title>
+<link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+<script src="js/bootstrap/bootstrap.min.js"></script>
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+<script src="js/bootstrap/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container">
+    
+  </div>
+</body>
+
+<!--<body background="BGG08.jpg" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0"><table width="1008" border="1" align="center">
+  <tbody>
+    <tr>
+      <td height="115" colspan="4" align="center"><img src="moo.png" width="966" height="99" alt=""/></td>
+    </tr>
+    <tr>
+      <td width="200" align="center" valign="top"><p><a href="123.png"><img src="123.png" alt="" width="65" height="65"/></a></p>
+        <p><a href="index.php" title="首頁" target="_top">首頁</a> </p>
+        <p>&nbsp;</p>
+        <p><a href="fabpu.html" title="租借須知">租借須知</a></p>
+        <p>&nbsp;</p>
+        <p style="font-size: 16px"><a href="uasti.html">關於我們</a></p>
+        <p>&nbsp;</p>
+        <p><a href="syuip.html" title="租借設備器材表">租借設備器材</a></p>
+        <p>&nbsp;</p>
+        <p><a href="prga.html">近期新上架商品</a></p>
+        <p>&nbsp; </p>
+        <p><a href="gope.html" title="GOO表單租借表">GOO表單租借表</a></p>
+        <p>&nbsp; </p>
+      <p><a href="nraku.html" title="歸還器材評價表">歸還器材評價表</a></p></td>
+      <td width="792" height="787" colspan="3" valign="top"><div id="201"><marquee><span style="font-size: 24px">本網站僅測試階段,非商業用途,待正式營運</span><marquee></marquee>
+        <figure> </figure>
+      </marquee></div>
+        <table width="908" height="626" border="1" align="center">
+          <tbody>
+          <tr>
+            <td height="33">最新上架商品圖示&amp;新上架商品</td>
+            </tr>
+          <tr>
+            <td width="856" height="585"><table width="903" height="257" border="1">
+              <tbody>
+                <tr>
+                  <td width="179"><img src="圖片/DJI MAVIC PRO.jpg" width="141" height="103" alt=""/></td>
+                  <td width="180"><img src="圖片/SPARK空拍機.jpg" width="140" height="103" alt=""/></td>
+                  <td width="184"><img src="圖片/3.jpg" width="163" height="142" alt=""/></td>
+                  <td width="200"><img src="圖片/1.jpg" width="159" height="137" alt=""/></td>
+                  </tr>
+                <tr>
+                  <td height="41">MAVIC PRO</td>
+                  <td>SPAPK</td>
+                  <td>LED燈</td>
+                  <td>LED燈配件電池</td>
+                  </tr>
+                <tr>
+                  <td height="38">售租$1200</td>
+                  <td>售租$1000</td>
+                  <td>售租LED燈(不含電池)$700</td>
+                  <td>售租配件$500</td>
+                  </tr>
+                </tbody>
+              </table>
+              <p>&nbsp;</p>
+              <table width="903" border="1">
+                <tbody>
+                  <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                  </tr>
+                  <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                  </tr>
+                </tbody>
+        </table>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p></td>
+            </tr>
+        </tbody>
+  </table>
+        <p style="text-align: center; font-size: 16px; font-family: '微軟正黑體 Light', '新細明體-ExtB';">聯絡方式:line&amp;fb&amp;電話<br>
+      住址:彰化縣員林市中正路二巷271弄14號<br>
+      電話:0987586999<br>
+      LINE ID:aa09331<br>
+      營業時間:每周一~周六上午10點~晚上11:30<br>
+      租借及歸還辦理時間:每周六.日上午9點~下午2點</p></td>
+    </tr>
+    <tr>
+      <td height="20" colspan="4"><span style="color: #000000;"><marquee>本皇翔租借商場不定期更新維護,本租借都使用表單,請當個有信用的租借者,聯絡方式 LINE:aa09331 </marquee></span></td>
+    </tr>
+  </tbody>
+</table>
+<h1>&nbsp;</h1>
+-->
