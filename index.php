6ff32e353aa905ac30faf4f3d6f65ce197f8e2d6
barrychen0507 <36660571+barrychen0507@users.noreply.github.com>
2019-03-27 15:44:09 +0530

    changed servreal documents :D\

--- index.php
+++ index.php
@@ -1,13 +1,67 @@
 ﻿<head>
 <title>皇翔租借商場 - 首頁</title>
-<link rel="stylesheet" href="css/bootstrap/bootstrap.css">
-<script src="js/bootstrap/bootstrap.min.js"></script>
-<script src="js/jquery-3.2.1.slim.min.js"></script>
-<script src="js/bootstrap/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="css/default.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
-<body>
-  <div class="container">
-    
+<body background="img/index_bg.jpg" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="font-family:微軟正黑體;">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #FF4500;">
+        <a class="navbar-brand" href="#">皇翔</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"><b>首頁</b> <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">租借須知</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">近期新上架商品</a>
+                </li>
+                <li class="nav-item active">
+                  <a href="#" class="nav-link">歸還器材評價表</a>
+                </li>
+                <li class="nav-item dropdown active">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">表單</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="#">GOO 租借表</a>
+                        <a class="dropdown-item" href="#">設備 租借表 2</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+    <div class="row">
+      <marquee><span style="font-size: 24px">本網站僅測試，正在開發階段，並非商業用途，僅供參考，待正式營運</span></marquee>
+    </div>
+    <div class="row">
+    <div class="card col-sm-1 col-md-3 col-lg-3" style="background-color:cadetblue; border-color:darkblue; height:550px;">
+      <img class="card-img-top margin-top-2" src="img/DJI MAVIC PRO.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">DJI MAVIC PRO</h4>
+        <p class="card-text">一種空拍機</p>
+      </div>
+    </div>
+    <div class="card col-sm-1 col-md-3 col-lg-3 margin-left-2" style="background-color:cadetblue; border-color:darkblue; height:550px;">
+      <img class="card-img-top margin-top-2" src="img/SPARK空拍機.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">DJI MAVIC PRO</h4>
+        <p class="card-text">一種空拍機</p>
+      </div>
+    </div>
+    <div class="card col-sm-1 col-md-3 col-lg-3 margin-left-2" style="background-color:cadetblue; border-color:darkblue; height:550px;">
+      <img class="card-img-top margin-top-2" src="img/3.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">DJI MAVIC PRO</h4>
+        <p class="card-text">一種空拍機</p>
+      </div>
+    </div>
+    </div>
   </div>
 </body>
 
